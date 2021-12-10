7fc70465c69424e052d79a2bde0a35eb6208614a
LucasOJacintho <lucasoliveirajacintho@gmail.com>
2021-12-10 08:22:33 -0300

    Conclusão da segunda aula - Ciração de projeto no Visual Studio e Variáveis

---
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade = 28;
+
+            Console.WriteLine(idade);
+
+            idade -= 10;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Pressione Enter para encerrar . . .");
+            Console.ReadLine();
+        }
+    }
+}
