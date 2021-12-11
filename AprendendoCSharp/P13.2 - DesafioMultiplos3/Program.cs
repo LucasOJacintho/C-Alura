f62b6d242033a8eaf5a987bb78483c8bb6b812fa
LucasOJacintho <lucasoliveirajacintho@gmail.com>
2021-12-11 13:30:36 -0300

    Desafio múltiplos de 3

---
+++ AprendendoCSharp/P13.2 - DesafioMultiplos3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13._2___DesafioMultiplos3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto - Desfaio Múltiplos de 3");
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para encerrar...");
+
+            Console.ReadLine();
+        }
+    }
+}
