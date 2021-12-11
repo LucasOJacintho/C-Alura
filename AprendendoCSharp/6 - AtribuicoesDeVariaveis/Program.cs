1534429a512efd67689145883b3b3b232fad4e44
LucasOJacintho <lucasoliveirajacintho@gmail.com>
2021-12-11 11:17:17 -0300

    Atividade 6 - treino com variáveis

---
+++ AprendendoCSharp/6 - AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6___AtribuicoesDeVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ecução do projeto 6 - Atribuições de Variáveis");
+
+            int idade = 29;
+            int idadeLucas = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeLucas);
+
+            Console.WriteLine("Pressione Enter para encerrar a execução do programa . . . ");
+
+            Console.ReadLine();
+        }
+    }
+}
