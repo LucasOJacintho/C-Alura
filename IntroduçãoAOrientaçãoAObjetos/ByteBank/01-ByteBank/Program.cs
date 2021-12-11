3de88d50509b7bbd887aad19bb030d2dea045ce4
LucasOJacintho <lucasoliveirajacintho@gmail.com>
2021-12-11 17:28:48 -0300

    Inicio do Curso de orientação à objeto

---
+++ IntroduçãoAOrientaçãoAObjetos/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoLucas = new ContaCorrente();
+
+            contaDoLucas.titular = "Lucas Oliveira";
+            contaDoLucas.agencia = 863;
+            contaDoLucas.numero = 863452;
+            contaDoLucas.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDoLucas.titular);
+            Console.WriteLine("Agência: " + contaDoLucas.agencia);
+            Console.WriteLine("Número: " + contaDoLucas.numero);
+            Console.WriteLine("Saldo: " + contaDoLucas.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
