266cd8c94a818b50db60eac8e003390f73d4aaca
LucasOJacintho <lucasoliveirajacintho@gmail.com>
2021-12-11 13:17:16 -0300

    Aula de For com utilização de break

---
+++ AprendendoCSharp/P13 - ForEncadeado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13___ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 13 - For Encadeado");
+
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Console.Write("*");
+               Console.WriteLine();
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para encerrar");
+
+            Console.ReadLine();
+        }
+    }
+}
