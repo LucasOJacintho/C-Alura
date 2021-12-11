59757bf6ed03892d2882f0d37138879adb1c6931
LucasOJacintho <lucasoliveirajacintho@gmail.com>
2021-12-11 11:52:06 -0300

    Aula 9 - Escopo, visto questao de variável pai e filho

---
+++ AprendendoCSharp/9 - Escopo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9___Escopo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando Projeto 9 - Escopo");
+
+            int idade = 18;
+            bool acompanhado = false;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+                mensagemAdicional = "Você está acompanhado";
+            else
+                mensagemAdicional = "Você não está acompanhado";
+
+            // Quando existe apenas uma linha de instrução não precisa de chaves
+         
+            Console.WriteLine(mensagemAdicional);
+
+            if (idade >= 18 || acompanhado == true)
+                Console.WriteLine("Você pode entrar");
+            else
+                Console.WriteLine("Você não pode entrar");
+
+            Console.WriteLine("Pressione qualquer tecla para o encerramento do programa...");
+
+            Console.ReadLine();
+
+        }
+    }
+}
